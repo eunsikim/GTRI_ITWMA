2b25be362083ea39a8eb3698d6164c0f46683123
eunsikim <kimeunsik229@gmail.com>
2022-10-13 10:28:52 -0400

    Abstracted the header file

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <header>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-        <link href="/css/bootstrap.min.css" rel="stylesheet">
-        <script defer src="js/bootstrap.bundle.min.js"></script>
-        <title><?php echo htmlspecialchars($title) ?></title>
-    </header>
-    <body>
-        <?php require(__DIR__.'/navbarHorizontal.php') ?>
-    
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <script defer src="js/bootstrap.bundle.min.js"></script>
+    <title><?php echo htmlspecialchars($title) ?></title>
+</head>
