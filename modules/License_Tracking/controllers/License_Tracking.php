9978126217d4629abafda49058272807df8b4422
eunsikim <kimeunsik229@gmail.com>
2022-11-10 18:13:40 -0500

    Fixed License_Tracking

--- modules/License_Tracking/controllers/License_Tracking.php
+++ modules/License_Tracking/controllers/License_Tracking.php
@@ -2,14 +2,14 @@
     
     require_once($_SERVER['DOCUMENT_ROOT']."/php/authentication/authentication.php");
 
-    $title = 'sampleApp';
+    $title = 'License Tracking';
     // If the user is not logged in, redirect to login view
 
     if(!isLogged()){
         header('Location: sampleApp');
     }
 
-    echo $_SESSION['TWIG'] ->render('/modules/sampleApp/views/sampleApp.html', [
+    echo $_SESSION['TWIG'] ->render('/modules/License_Tracking/views/License_Tracking.html', [
         'isLogged' => isLogged(),
         'title' => $title,
         'userName' => $_SESSION['user'],
