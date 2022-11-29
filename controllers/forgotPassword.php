173256854ad135f33df672fd6222f381b238a4a5
Catlinw <washington.e.catlin@gmail.com>
2022-11-29 16:35:14 -0500

    dash

--- controllers/forgotPassword.php
+++ controllers/forgotPassword.php
@@ -1,14 +1,14 @@
 <?php
-    // $title = 'Forgot Password';
+    $title = 'Forgot Password';
     
-    // echo $_SESSION['TWIG']->render('/views/forgotPassword.html', [
-    //     'title' => $title,
-    //     // 'userName' => $_SESSION['user'],
-    //     // 'isLogged' => isLogged(),
-    //     // 'admin' => $_ENV['USER_TYPE'],
-    //     'appName' => $_ENV['APP_NAME']
-    //     // 'userType' => $_ENV['USER_TYPE'],
-    //     // 'admin' => isAdmin()
-    // ]);
+    echo $_SESSION['TWIG']->render('/views/forgotPassword.html', [
+        'title' => $title,
+        // 'userName' => $_SESSION['user'],
+        // 'isLogged' => isLogged(),
+        // 'admin' => $_ENV['USER_TYPE'],
+        'appName' => $_ENV['APP_NAME']
+        // 'userType' => $_ENV['USER_TYPE'],
+        // 'admin' => isAdmin()
+    ]);
 
-    echo('Hello World');
+    // echo('Hello World');
