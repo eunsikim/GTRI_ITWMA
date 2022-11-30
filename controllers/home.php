45f3f38f845ed511b1c81c5231da20d8cbaeb56d
eunsikim <kimeunsik229@gmail.com>
2022-11-30 03:03:15 -0500

    Added Chart.js. Implemented Widget Loading System.

--- controllers/home.php
+++ controllers/home.php
@@ -1,6 +1,7 @@
 <?php
     require_once($_SERVER['DOCUMENT_ROOT'].'/php/authentication/authentication.php');
     require_once($_SERVER['DOCUMENT_ROOT'].'/php/authentication/roles.php');
+    require_once($_SERVER['DOCUMENT_ROOT']."/mysql/config.php");
 
     $title = 'Home';
 
@@ -9,11 +10,20 @@
         header('Location: login');
     }
 
+    // Initialize widget array
+    $widgetData = array();
+    //Load dashboard Widget Controllers
+    foreach($_SERVER['MODULE_PATHS'] as $module){
+        require($module[3]);
+    }
+    
+
     echo $_SESSION['TWIG']->render('/views/home.html', [
         'title' => $title, //Expected by the header
         'userName' => $_SESSION['current_user']['firstName'], //Expected for nav bar user's name display
         'userView' => checkPrivilege('view_users', $_SESSION['user_roles']), //Expected for nav bar to show (or not) the users table view
         'rolesView' => checkPrivilege('view_roles', $_SESSION['user_roles']),
         'appName' => $_ENV['APP_NAME'], //Expected for nav bar to show name of the application
-        'modules' => $_SERVER['MODULE_PATHS'], //Expected side navbar
+        'modules' => $_SERVER['MODULE_PATHS'], //Expected side navbar,
+        'widgetData' => $widgetData
     ]); 
